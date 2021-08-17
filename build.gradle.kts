ca9e91fbb19c9bec3cd68a58f8c42b59ae606ca4
Valdeci Gomes <valfilho@amazon.com>
2021-08-17 10:25:50 -0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.5.21"
+}
+
+group = "ci.valde"
+version = "0.0.1"
+application {
+    mainClass.set("ci.valde.ApplicationKt")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.10.3")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlin_version")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ktor-content-negotiation-xml"
